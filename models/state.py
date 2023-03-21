3869479ecc8afcf8b08301348f83bb30fb6a92b5
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2023-03-21 15:10:22 +0000

    Update class model for State using SQLAlchemy

--- models/state.py
+++ models/state.py
@@ -2,8 +2,13 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete, delete_orphan',
+                          backref='State')
