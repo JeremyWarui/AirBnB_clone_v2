2b4210938d09e97e5286b0e32771e536d9c12143
JeremyWarui <jerywarne7@gmail.com>
2023-03-21 23:06:40 +0300

    Update the delete function

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models import storage
+import models
 # enable sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
@@ -44,10 +44,10 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        
+        from models import storage
         self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -62,4 +62,4 @@ class BaseModel:
         return dictionary
 
     def delete(self):
-        storage.delete(self)
+        models.storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from models.state import State
+# from models.state import State
 
 
 class City(BaseModel, Base):
