8b54e6e58df00fb961b783558e3d1f7f3105d01f
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2023-03-22 07:23:12 +0000

    Update classes for relationship in line with SQLAlchemy

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 # from models.state import State
 
 
@@ -10,3 +11,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', cascade='all, delete, delete-orphan',
+                          backref='cities')
