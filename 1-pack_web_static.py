9c090691f2d1b9afad6ffc56606e584ea6897219
JeremyWarui <jerywarne7@gmail.com>
2023-04-10 00:18:11 +0300

    pack web files to tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the
+contents of the web_static folder
+"""
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """
+    function that packs the contents of web_Static to tgz archive
+    """
+    tym = datetime.now().strftime("%Y%m%d%H%M%S%")
+
+    try:
+        local("mkdir -p versions")
+        archive_file = "versions/web_static_{}.tgz".format(tym)
+        local("tar -cvzf {} web_static/".format(archive_file))
+        return archive_file
+    except Exception:
+        return None
