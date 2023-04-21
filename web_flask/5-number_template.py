2c2302609308bf10f2470bd5ba98cf960d34b02c
JeremyWarui <jerywarne7@gmail.com>
2023-04-21 21:50:46 +0300

    Different routes for the app

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def number_route(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """displays 5-number templace if 'n' is an integer"""
+    """displays 5-number template if 'n' is an integer"""
     return render_template("5-number.html", number=n)
 
 
