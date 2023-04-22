ae4b0b24afe95d38e8fb2390a93b182e304834cf
JeremyWarui <jerywarne7@gmail.com>
2023-04-22 20:36:28 +0300

    States list template

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that  starts Flask app
+Listens on 0.0.0.0 port 5000
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_session(self):
+    """tear down the sqlalchemy session """
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def states_list():
+    """displays all states"""
+    states = storage.all(State)
+    list_of_states = [state for state in states.values()]
+    return render_template("8-cities_by_states.html",
+                           list_of_states=list_of_states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
