11c16a63da8853bcf65c0843c67cc158324907b6
JeremyWarui <jerywarne7@gmail.com>
2023-04-23 20:58:28 +0300

    Advanced task

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -7,6 +7,8 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.city import City
+from models.place import Place
 
 
 app = Flask(__name__)
@@ -18,15 +20,20 @@ def teardown_session(self):
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb_filters():
     """dynamically display templates"""
     amenities_list = storage.all(Amenity)
     states_list = storage.all(State)
+    places_list = storage.all(Place)
+
     amenities = [amenity for amenity in amenities_list.values()]
     states = [state for state in states_list.values()]
-    return render_template("10-hbnb_filters.html",
-                           states=states, amenities=amenities)
+    places = [place for place in places_list.values()]
+
+    return render_template("100-hbnb.html",
+                           states=states, amenities=amenities,
+                           places=places)
 
 
 if __name__ == "__main__":
